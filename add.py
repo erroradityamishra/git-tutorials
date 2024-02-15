505a1f1f3aa498fb028a4549839ede60e0ba20f5
erroradityamishra <mishrajiaditya75@gmail.com>
2024-02-15 15:37:30 +0530

    added a line

--- add.py
+++ add.py
@@ -1,4 +1,4 @@
-a= input ("Enter the no ")
-b= input ("Enter the no 2")
+a = input("Enter the no: ")
+b = input("Enter the no 2: ")
 sum = int(a) + int(b)
-print (sum)
+print("And the answer is:",sum)
